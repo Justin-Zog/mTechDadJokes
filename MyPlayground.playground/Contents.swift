19caf76a6b820aff277bdf7be85681325c710ff2
Justin Herzog <justinherzog@Justins-MacBook-Pro.local>
2018-08-27 09:53:54 -0600

    First Dad Joke

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+print("A sandwhich walks into a bar and asks for a beer.  The bartender says, Sorry, we don't serve food here.")
